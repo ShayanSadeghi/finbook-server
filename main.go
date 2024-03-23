49df551444f04a8e52cfad9b22a2fbd23cd95e0c
Shayan Sadeghi <ShayanSadeghi1996@gmail.com>
2024-03-23 12:55:25 +0330

    use gorilla/mux to handle requests

--- main.go
+++ main.go
@@ -1,10 +1,22 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"log"
 	"net/http"
+
+	"github.com/gorilla/mux"
 )
+
+type User struct {
+	ID       uint   `json:"id"`
+	Username string `json:"username"`
+	Email    string `json:"email"`
+	Password string `json:"password"`
+}
+
+var users []User
 
 func helloHandler(w http.ResponseWriter, r *http.Request) {
 	if r.Method == "GET" {
@@ -12,11 +24,20 @@ func helloHandler(w http.ResponseWriter, r *http.Request) {
 	}
 }
 
+func getUsers(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "Application/json")
+	json.NewEncoder(w).Encode(users)
+}
+
 func main() {
-	http.HandleFunc("/hello", helloHandler)
+	r := mux.NewRouter()
+
+	users = append(users, User{ID: 1, Username: "Shayan", Email: "shayan@mail.com", Password: "1234"})
+	users = append(users, User{ID: 2, Username: "Sara", Email: "sara@mail.com", Password: "1234"})
+
+	r.HandleFunc("/hello", helloHandler).Methods("GET")
+	r.HandleFunc("/users", getUsers).Methods("GET")
 
 	fmt.Println("App is running on port 8000")
-	if err := http.ListenAndServe(":8000", nil); err != nil {
-		log.Fatal(err)
-	}
+	log.Fatal(http.ListenAndServe(":8000", r))
 }
